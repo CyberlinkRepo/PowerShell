914baf643fb27c6407cd2474a97f0898e4b5fdd8
Glen Godinho <119540117+CyberlinkRepo@users.noreply.github.com>
2022-12-26 13:45:46 +0000

    Add files via upload

---
+++ Setup.ps1
@@ -0,0 +1,5 @@
+﻿# Download PowerShell 7 INstalltion Script
+Set-Location C:\Powershell_Download
+$URI = "https://aka.ms/install-powershell.ps1"
+Invoke-RestMethod -Uri $URI |
+Out-File -FilePath C:\Powershell_Download\Install-PowerShell.ps1
---
+++ Walkthrough_1.ps1
@@ -0,0 +1,8 @@
+﻿# Check the existing version
+$PSVersiontable
+# Set an execution policy
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+# Install Nuget as a package provider
+Install-packageProvider Nuget -MinimumVersion 2.8.5.201 -Force | Out-Null
+# Install the module
+Install-Module -Name PowerShellGet -Force -AllowClobber
